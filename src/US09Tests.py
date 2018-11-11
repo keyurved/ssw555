c4f5c253e130c2d9b072cedfe1af172f44169957
Monica Razak <monicarazak35@gmail.com>
2018-11-11 11:40:49 -0500

    Added unit tests of US09 US10 US17 US18

--- src/US09Tests.py
+++ src/US09Tests.py
@@ -1,10 +1,11 @@
+
 import unittest
 from Family import Family
 from Individual import Individual
 
-class MarriageOfSiblings(unittest.TestCase):
+class TestBirthBeforeDeathOfParents(unittest.TestCase):
 
-    def test_valid_marriages(self):
+  def test_invalid_birth(self):
         p1_dict = { 'INDI': 'I0',
                 'NAME': 'Person /One',
                 'SEX': 'M',
@@ -15,6 +16,7 @@ class MarriageOfSiblings(unittest.TestCase):
                 'NAME': 'Person /Two',
                 'SEX': 'F',
                 'BIRT': '11 Aug 1973',
+                'DEAT': '12 Aug 1999',
                 'FAM': 'F0'
                 }
         p3_dict = { 'INDI': 'I2',
@@ -41,9 +43,9 @@ class MarriageOfSiblings(unittest.TestCase):
                 'CHIL': [child1,child2],
         }
 
-        self.assertFalse(Family.instance_from_dict(fam_dict).anomalies)
+        self.assertTrue(Family.instance_from_dict(fam_dict).errors)
     
-    def test_marriage_invalid(self):
+  def test_valid_birth(self):
         p1_dict = { 'INDI': 'I0',
                 'NAME': 'Person /One',
                 'SEX': 'M',
@@ -54,41 +56,35 @@ class MarriageOfSiblings(unittest.TestCase):
                 'NAME': 'Person /Two',
                 'SEX': 'F',
                 'BIRT': '11 Aug 1973',
+                'DEAT': '12 Aug 2002',
                 'FAM': 'F0'
                 }
         p3_dict = { 'INDI': 'I2',
                 'NAME': 'Pperson /One',
                 'SEX': 'F',
                 'BIRT':'23 Apr 2000',
-                'FAM': ['F0','F1'] 
+                'FAM': 'F0'
                 }
         p4_dict = { 'INDI': 'I3',
                 'NAME': 'Personn /One',
                 'SEX': 'M',
                 'BIRT':'20 Mar 2001',
-                'FAM': ['F0','F1']
+                'FAM': 'F0'
                 }                
 
-        husband1 = Individual.instance_from_dict(p1_dict)
-        wife1 = Individual.instance_from_dict(p2_dict)
+        husband = Individual.instance_from_dict(p1_dict)
+        wife = Individual.instance_from_dict(p2_dict)
         child1 = Individual.instance_from_dict(p3_dict)
-        child2 = Individual.instance_from_dict(p4_dict)       
+        child2 = Individual.instance_from_dict(p4_dict)        
         fam_dict = { 'FAM': 'F0',
-                'HUSB': husband1,
-                'WIFE': wife1,
+                'HUSB': husband,
+                'WIFE': wife,
                 'MARR': '15 Mar 1994',
                 'CHIL': [child1,child2],
         }
-        fam_dict2 = { 'FAM': 'F1',
-                'HUSB': child2,
-                'WIFE': child1,
-                'MARR': '15 Jul 2018',
-        }
-        Family.instance_from_dict(fam_dict2)
-        self.assertTrue(Family.instance_from_dict(fam_dict).anomalies)
-        
 
+        self.assertFalse(Family.instance_from_dict(fam_dict).errors)
+    
 
 if __name__ == '__main__':
-    #unittest.main()
-    unittest.main(argv=['first-arg-is-ignored'], exit=False)
+    unittest.main()
